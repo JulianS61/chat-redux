5f4cf2280cf686e3cfef2d0d9f92ec18398083ab
Julian Segrestan <julian.nihon@gmail.com>
2021-03-30 06:45:37 +0900

    managed to make the list scroll to last message

--- src/containers/messageList.jsx
+++ src/containers/messageList.jsx
@@ -12,32 +12,44 @@ class MessageList extends Component {
   constructor(props) {
     super(props);
     this.state = { interval: null };
+    this.listScroll = null;
+    this.setListRef = (element) => { this.listScroll = element; };
+    this.scrollTopList = () => {
+      console.log(this.listScroll.scrollTop);
+      if (this.listScroll) this.listScroll.scrollTop = this.listScroll.scrollHeight;
+    };
   }
+
   componentWillMount() {
     this.props.fetchMessages('467');
   }
 
-  // componentDidMount() {
-  //   const interval = setInterval(() => { this.props.fetchMessages('467'); }
-  //     , 1000);
-  //   // eslint-disable-next-line react/no-did-mount-set-state
-  //   this.setState({ interval });
-  // }
+  componentDidMount() {
+    const interval = setInterval(() => { this.props.fetchMessages('467'); }
+      , 10000);
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({ interval });
+  }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.state.interval);
-  // }
+  componentDidUpdate() {
+    this.scrollTopList();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
 
   render() {
     const { messages } = this.props;
     return (
-      <div className="message-list">
+      <div className="message-list" onChange={this.scrollTopList}>
         <div>
           <h2><FontAwesomeIcon icon={['fab', 'slack-hash']} /> ch1</h2>
           <hr />
         </div>
         <div>
-          <div className="scroll">
+          <div className="scroll" ref={this.setListRef}>
             {messages.map(message => <Message key={message.id} message={message} />)}
           </div>
           <hr />
